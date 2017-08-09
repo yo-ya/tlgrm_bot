e14a0635e3b8f2d0c1e3d56f7a1d4a69e02da494
yo-ya <rinat.makh@gmail.com>
2017-08-09 10:02:25 +0700

    add
    add

---
+++ tlgrm_bot/tlgrm_bot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tlgrm_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
